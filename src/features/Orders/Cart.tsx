f576f4d34f04081accf8094c89bdc8c755c2a83c
Brady Richmond <richmond.brady@gmail.com>
2023-01-15 13:09:16 -0800

    updated cart

--- src/features/Orders/Cart.tsx
+++ src/features/Orders/Cart.tsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { CartItem, selectCartItems } from './CartSlice';
+import { CartItem, removeCartItem, selectCartItems, updateItem } from './CartSlice';
 import { Storage } from 'aws-amplify';
 import { useLegoData } from '../../hooks/legoHooks';
+import { Counter } from '../Stores/StoreData';
+import { selectCurrentTrip } from '../Trips/TripsSlice';
 
 const Cart = () => {
     const cartItemList = useAppSelector(selectCartItems);
+    const tripData = useAppSelector(selectCurrentTrip);
+    const perCup = parseFloat(tripData?.cupPrice ?? '0');
+    const cupCount = cartItemList.reduce((prev, curr) => prev + curr.count, 0);
+    const subtotal = cupCount * perCup;
+    const shippingUnits = Math.ceil(cupCount / 3);
+    const shippingCost = parseFloat(tripData?.shippingPrice ?? '') * shippingUnits;
+    const total = parseFloat(shippingCost.toString()) + subtotal;
+
     return (
         <Box>
             {
@@ -16,13 +26,28 @@ const Cart = () => {
                     )
                 })
             }
+            <Box borderTop='2px solid rgba(246,236,54,255)' padding='2rem'>
+                <Box display='flex' flexDirection='row'>
+                    <Box flex='1'>Subtotal:</Box>
+                    <Box flex='1'>${subtotal}</Box>
+                </Box>
+                <Box display='flex' flexDirection='row'>
+                    <Box flex='1'>Shipping:</Box>
+                    <Box flex='1'>${shippingCost}</Box>
+                </Box>
+                <Box display='flex' flexDirection='row'>
+                    <Box flex='1'>Total:</Box>
+                    <Box flex='1'>${total}</Box>
+                </Box>
+            </Box>
         </Box>
     )
 }
 
 const Item = (props: { cartItem: CartItem }) => {
     const { cartItem } = props;
-    const [url, setUrl] = useState('')
+    const [url, setUrl] = useState('');
+    const dispatch = useAppDispatch();
 
     const legoData = useLegoData(cartItem.itemId);
 
@@ -37,9 +62,24 @@ const Item = (props: { cartItem: CartItem }) => {
         }
     }, [legoData?.imageKey])
 
+    const addItem = () => {
+        dispatch(updateItem({itemId: cartItem.itemId, itemCount: cartItem.count + 1}));
+    }
+
+    const subtractItem = () => {
+        if (cartItem.count === 1) {
+            dispatch(removeCartItem(cartItem.itemId));
+        }
+
+        dispatch(updateItem({itemId: cartItem.itemId, itemCount: cartItem.count - 1}));
+    }
+
     return (
-        <Box border='2px solid rgba(246,236,54,255)' borderRadius='1rem' padding='2rem' margin='2rem'>
-            <img src={url} alt="" width='200px' />
+        <Box border='2px solid rgba(246,236,54,255)' borderRadius='1rem' padding='2rem' margin='2rem' display='flex' flexDirection='row'>
+            <Box><img src={url} alt="" width='200px' /></Box>
+            <Box display='flex' flex='1' alignItems='center' justifyContent='flex-end'>
+                <Counter itemCount={cartItem.count} addAction={addItem} minusAction={subtractItem} />
+            </Box>
         </Box>
     )
 }
